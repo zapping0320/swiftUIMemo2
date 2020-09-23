5caf90f39df4e2048f0354ab43d93253c15c537c
John DongHyun Kim <zapping0320@gmail.com>
2020-09-24 06:56:25 +0900

    Initial Commit

---
+++ swiftUIMemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIMemo2
+//
+//  Created by DONGHYUN KIM on 2020/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
