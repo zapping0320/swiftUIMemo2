177744c9d9f0d35ace3c180804601795926e33cb
John DongHyun Kim <zapping0320@gmail.com>
2020-09-24 07:06:23 +0900

    프로젝트 설정

--- swiftUIMemo2/MemoListScene.swift
+++ swiftUIMemo2/MemoListScene.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MemoListScene: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MemoListScene_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MemoListScene()
     }
 }
