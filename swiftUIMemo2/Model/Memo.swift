ce7e2d50555d68d2403f647dde8c1670870ebddc
John DongHyun Kim <zapping0320@gmail.com>
2020-09-25 07:04:00 +0900

    Memo 구현

--- swiftUIMemo2/Model/Memo.swift
+++ swiftUIMemo2/Model/Memo.swift
@@ -8,5 +8,15 @@
 import SwiftUI
 
 class Memo : Identifiable, ObservableObject {
+    let id:UUID
+    @Published var content : String
+    let insertDate : Date
+    
+    init(id : UUID = UUID(), content:String,
+         insertDate : Date = Date()) {
+        self.id = id
+        self.content = content
+        self.insertDate = insertDate
+    }
     
 }
