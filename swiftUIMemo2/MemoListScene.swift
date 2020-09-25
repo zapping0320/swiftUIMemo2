8b1ac1d73be768049d933420978953baf8afcee9
John DongHyun Kim <zapping0320@gmail.com>
2020-09-26 08:39:44 +0900

    MemoCell로 extract subview함

--- swiftUIMemo2/MemoListScene.swift
+++ swiftUIMemo2/MemoListScene.swift
@@ -14,15 +14,7 @@ struct MemoListScene: View {
     var body: some View {
         NavigationView {
             List(store.list) { memo in
-                VStack(alignment: .leading) {
-                    Text(memo.content)
-                        .font(.body)
-                        .lineLimit(1)
-                    
-                    Text("\(memo.insertDate, formatter: self.formatter)")
-                        .font(.caption)
-                        .foregroundColor(Color(UIColor.secondaryLabel))
-                }
+                MemoCell(memo: memo)
             }
             .navigationBarTitle("내 메모")
         }
@@ -34,5 +26,22 @@ struct MemoListScene_Previews: PreviewProvider {
         MemoListScene()
             .environmentObject(MemoStore())
             .environmentObject(DateFormatter.memoDateFormatter)
+    }
+}
+
+struct MemoCell: View {
+    @ObservedObject var memo:Memo
+    @EnvironmentObject var formatter:DateFormatter
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body)
+                .lineLimit(1)
+            
+            Text("\(memo.insertDate, formatter: self.formatter)")
+                .font(.caption)
+                .foregroundColor(Color(UIColor.secondaryLabel))
+        }
     }
 }
