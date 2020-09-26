eb2f483f99aa94a46ab574fa59e4a39741817178
John DongHyun Kim <zapping0320@gmail.com>
2020-09-27 07:18:29 +0900

    #7 화면 이동

--- swiftUIMemo2/Scene/ComposeScene.swift
+++ swiftUIMemo2/Scene/ComposeScene.swift
@@ -11,6 +11,8 @@ struct ComposeScene: View {
     @EnvironmentObject var store:MemoStore
     @State private var content: String = ""
     
+    @Binding var showComposer:Bool
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -18,16 +20,18 @@ struct ComposeScene: View {
             }
             .frame(maxWidth: .infinity, maxHeight:  .infinity)
             .navigationBarTitle("새 메모", displayMode: .inline)
-            .navigationBarItems(leading: DismissButton(), trailing: SaveButton())
+            .navigationBarItems(leading: DismissButton(show: $showComposer), trailing: SaveButton(show: $showComposer))
             
         }
     }
 }
 
 fileprivate struct DismissButton : View {
+    @Binding var show:Bool
+    
     var body : some View {
         Button(action: {
-            
+            self.show = false
         },
         label: {
             Text("취소")
@@ -36,9 +40,11 @@ fileprivate struct DismissButton : View {
 }
 
 fileprivate struct SaveButton : View {
+    @Binding var show:Bool
+    
     var body : some View {
         Button(action: {
-            
+            self.show = false
         },
         label: {
             Text("저장")
@@ -48,7 +54,7 @@ fileprivate struct SaveButton : View {
 
 struct ComposeScene_Previews: PreviewProvider {
     static var previews: some View {
-        ComposeScene()
+        ComposeScene(showComposer: .constant(false))
             .environmentObject(MemoStore())
     }
 }
--- swiftUIMemo2/Scene/MemoListScene.swift
+++ swiftUIMemo2/Scene/MemoListScene.swift
@@ -11,13 +11,33 @@ struct MemoListScene: View {
     @EnvironmentObject var store:MemoStore
     @EnvironmentObject var formatter:DateFormatter
     
+    @State var showComposer: Bool = false
+    
     var body: some View {
         NavigationView {
             List(store.list) { memo in
                 MemoCell(memo: memo)
             }
             .navigationBarTitle("내 메모")
+            .navigationBarItems(trailing: ModalButton(show: $showComposer))
+            .sheet(isPresented: $showComposer, content : {
+                ComposeScene(showComposer: self.$showComposer)
+            })
         }
+    }
+}
+
+fileprivate struct ModalButton : View {
+    @Binding var show:Bool
+    
+    
+    var body : some View {
+        Button(action: {
+            self.show = true
+        },
+        label: {
+            Image(systemName: "plus")
+        })
     }
 }
 
